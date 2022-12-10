a5482756e17f531001cacbad126ddbe07bd4c450
stephgarro2001@gmail.com <109922003+Stephgarro@users.noreply.github.com>
2022-12-09 21:31:14 -0600

    i tried  to do the game of the life

---
+++ juego de la vida/juego.js
@@ -0,0 +1,44 @@
+// i try to coplete this but my code doesn`t work pretty well and i don`t know why! but i tried
+/*function matriz() {
+
+  function construir(tamaño, ctx) {
+
+    let tamaño = tamaño;
+    let ctx = ctx;
+    let casilla = [];
+  }
+
+ function inicializar() {
+
+  for (let i = 0; i < tamaño; i++) {
+   let filas= [];
+
+   for (let j = 0; j < tamaño; j++) {
+    const condicion = Math.random()<0.5;
+    filas.push(condicion);
+    
+   }
+
+   casilla.push(filas);
+    
+  }
+ }
+ console.log(ctx.clearRect(0,0,  tamaño, tamaño));
+ for (let i = 0; i < tamaño; i++) {
+  for (let j = 0; j < tamaño; j++) {
+   if (casilla[i][j]) {
+    ctx.fillStyle="black";
+   } else {
+    ctx.fillStyle="white";
+    ctx.fillRect(i,j, 1,1)
+   }
+    
+  }
+ }
+
+}
+
+const contexto = canvas.getContext('2d')
+const matriz = new matriz(100,ctx)
+console.log(matriz(inicializar()))
+console.log(matriz)*/
